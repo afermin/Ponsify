34dccc2bc2cb5471cdaffaf703f7ba020d32475b
System Administrator <root@MacBook-Pro-de-Alexander.local>
2018-06-11 01:31:38 +0200

    Main function was done

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 (function() {
 
-    function ponsify(x, y, z) {
-        // Add your code here
-        console.log("Hello world!");
-        return -1;
+    function ponsify(lowerLimit, upperLimit, step) {
+      if (lowerLimit>upperLimit) {
+        return 0
+      }
+      if (lowerLimit===upperLimit) {
+        return lowerLimit
+      }
+      return ponsify(lowerLimit+step, upperLimit, step)+lowerLimit
     }
-    
+
     // required to export for tests
     // DO NOT TOUCH
     var exports = module.exports;
