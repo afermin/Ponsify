ab0fa8e07b5fc73eccafe2ff2bca9e8fca67df50
System Administrator <root@MacBook-Pro-de-Alexander.local>
2018-06-15 18:41:15 +0200

    ponsify function, which was improved, now has a constant time complexity

--- src/main.js
+++ src/main.js
@@ -1,17 +1,14 @@
-(function() {
+(function () {
 
-    function ponsify(lowerLimit, upperLimit, step) {
-      if (lowerLimit > upperLimit) {
-        return 0
-      }
-      if (lowerLimit === upperLimit) {
-        return lowerLimit
-      }
-      return ponsify(lowerLimit+step, upperLimit, step)+lowerLimit
-    }
-
-    // required to export for tests
-    // DO NOT TOUCH
-    var exports = module.exports;
-    module.exports = ponsify;
-})();
+  function ponsify (lowerLimit, upperLimit, step) {
+    var numberSteps = Math.floor((upperLimit - lowerLimit) / step)
+    var stepAccum = Math.floor(numberSteps * (numberSteps + 1) / 2)
+    var validator = (lowerLimit <= upperLimit)
+    var lowerLimitAccum = ((numberSteps + 1) * lowerLimit)
+    return (stepAccum * step + lowerLimitAccum) * validator
+  }
+  // required to export for tests
+  // DO NOT TOUCH
+  var exports = module.exports
+  module.exports = ponsify
+})()
--- test/test.js
+++ test/test.js
@@ -1,18 +1,18 @@
 var assert = require('assert');
 var ponsify = require('../dist/script.min.js');
 
-describe('Ponsify', function() {
-    it('should return the sum of integers between the lower and upper limits going in steps', function() {
-        assert.equal(55, ponsify(1, 10, 1));
-        assert.equal(26, ponsify(2, 12, 3));
-        assert.equal(18, ponsify(3, 9, 3));
-    });
-    it('should return X when the lower limit and upper limit are equal to X', function() {
-        assert.equal(3, ponsify(3, 3, 3));
-        assert.equal(2, ponsify(2, 2, 1));
-        assert.equal(1, ponsify(1, 1, 4));
-    });
-    it('should return 0 when the upper limit is less than the lower limit', function() {
-        assert.equal(0, ponsify(4, 3, 2));
-    });
-});
+describe('Ponsify', function () {
+  it('should return the sum of integers between the lower and upper limits going in steps', function () {
+    assert.equal(26, ponsify(2, 11, 3)); //(2, 11, 3)   9/3=3
+    assert.equal(55, ponsify(1, 10, 1)); //10x11=110/2=55
+    assert.equal(18, ponsify(3, 9, 3));  //3x3=9+9=18x9=162/(3x3)=18
+  })
+  it('should return X when the lower limit and upper limit are equal to X', function () {
+    assert.equal(3, ponsify(3, 3, 3));
+    assert.equal(2, ponsify(2, 2, 1));
+    assert.equal(167167, ponsify(1, 1001, 3));
+  })
+  it('should return 0 when the upper limit is less than the lower limit', function () {
+    assert.equal(0, ponsify(4, 3, 2));
+  })
+})
